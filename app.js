364bbe2f61f556d59090fd3c80c1bb9234c54cfa
Matt Leary <33221468+mleary@users.noreply.github.com>
2026-06-11 21:53:26 -0400

    update message to test sync

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Tottenham Hotspur are the best club in the world!' });
+    res.json({ message: 'Tottenham Hotspur are the best club in the world! COYS!' });
 });
 
 app.listen(port);
